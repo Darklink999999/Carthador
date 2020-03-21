8e69af70820c8fec420d4a2a893672b5f3892d79
Darklink999999 <stavros.kokkineas@gmail.com>
2020-03-22 01:41:02 +0200

    Player Light

--- Assets/Scripts/PlayerSpotlight.cs
+++ Assets/Scripts/PlayerSpotlight.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerSpotlight : MonoBehaviour
 {
 	private Game game;
+    private Light globalLight;
 	
     // Start is called before the first frame update
     void Start()
     {
-        game = Camera.main.GetComponent<Game>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<Light>().intensity = (game.globalLight.GetComponent<Light>().intensity + 1) * -1;
+        if(SceneManager.GetActiveScene ().name == "World" && (game == null || globalLight == null)) {
+
+
+            game = Camera.main.GetComponent<Game>();
+            globalLight = GameObject.Find("GlobalLight").GetComponent<Light>();
+        }
+
+
+        if (SceneManager.GetActiveScene().name == "World")
+            this.GetComponent<Light>().intensity = (- (globalLight.intensity - 0.999f) * 3) - 2.2f;
     }
 }
