8462c727956d6c468af270a44e06be3395ed54a0
Darklink999999 <stavros.kokkineas@gmail.com>
2020-03-22 02:11:13 +0200

    Fixes

---
+++ Assets/Scripts/PlayerSpotlight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerSpotlight : MonoBehaviour
+{
+    private Game game;
+    private Light globalLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (SceneManager.GetActiveScene().name == "World" && (game == null || globalLight == null))
+        {
+
+
+            game = Camera.main.GetComponent<Game>();
+            globalLight = GameObject.Find("GlobalLight").GetComponent<Light>();
+        }
+
+
+        if (SceneManager.GetActiveScene().name == "World")
+            this.GetComponent<Light>().intensity = (-(globalLight.intensity - 0.999f) * 3) - 2.2f;
+    }
+}
